79d5e50a2c35b8d0109c41349f92e73eca1eebda
ShaggyMunky <aaronguillen21@gmail.com>
2018-02-16 15:40:52 -0800

    created nav bar and base for all pages

---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = props => {
+    return (
+        <div className="center-align">
+            <h1>Welcom Movie quote</h1>
+            <h3>Lorem ipsum dolor sit amet, consectetur.</h3>
+        </div>
+    )
+};
+
+export default Home;
