b90a4fe408678fb840418a4a4c296d26eed526d1
ShaggyMunky <aaronguillen21@gmail.com>
2018-02-20 12:48:32 -0800

    completed nav bar sign in changes

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,7 +1,28 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import {connect} from "react-redux";
+import {signOut} from "../actions";
 
 const Nav = props => {
+
+    function renderLinks (){
+        if (props.auth){
+            return (
+                <li>
+                    <button className="btn amber" onClick={props.signOut}>Logout</button>
+                </li>
+            )
+        }
+        return [
+            <li key="0">
+                <Link to="/sign-in">Sign In</Link>
+            </li>,
+            <li key="1">
+                <Link to="/sign-up">Sign Up</Link>
+            </li>
+        ]
+    }
+
     return (
         <nav className="deep-purple darken-2">
             <Link style={{marginLeft: "8px"}} to="/" className="brand-logo">Movie Quotes</Link>
@@ -15,15 +36,15 @@ const Nav = props => {
                 <li>
                     <Link to="/movie-quote">Movie Quote</Link>
                 </li>
-                <li>
-                    <Link to="/sign-in">Sign In</Link>
-                </li>
-                <li>
-                    <Link to="/sign-up">Sign Up</Link>
-                </li>
+                {renderLinks()}
             </ul>
         </nav>
     )
 };
 
-export default Nav;
+function mapStateToProps(state){
+    return {
+        auth: state.user.auth
+    }
+}
+export default connect(mapStateToProps, {signOut})(Nav);
